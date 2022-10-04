ece5e77374a45e7eea10957481669e393e794533
abhi-mehta97 <85896208+abhi-mehta97@users.noreply.github.com>
2022-10-03 22:12:36 -0400

    Update install.sh

--- terraform_code/dev/instances/install.sh
+++ terraform_code/dev/instances/install.sh
@@ -5,4 +5,4 @@
     sudo yum install mysql -y
     sudo systemctl start docker
     sudo usermod -a -G docker ec2-user
-    docker network create -d bridge --subnet 185.18.0.1/24 --gateway 185.18.0.1 my-net
+    docker network create my-net
